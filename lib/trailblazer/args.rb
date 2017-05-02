3aa21db19810c1f92a0480317e00367a6c2e740d
Nick Sutterer <apotonick@gmail.com>
2017-05-02 20:11:36 +0200

    initial commit.

---
+++ lib/trailblazer/args.rb
@@ -0,0 +1,7 @@
+require "trailblazer/args/version"
+
+module Trailblazer
+  module Args
+    # Your code goes here...
+  end
+end
---
+++ lib/trailblazer/args/version.rb
@@ -0,0 +1,5 @@
+module Trailblazer
+  module Args
+    VERSION = "0.1.0"
+  end
+end
